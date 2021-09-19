13530e70d6e74bc2c07f9ec8bd8e4d7c599f4c11
Will Nyamunokora <wnnwillard@gmail.com>
2021-09-19 03:08:39 +0200

    Initialize clockings

--- src/components/clockings/ClockingsList.js
+++ src/components/clockings/ClockingsList.js
@@ -15,18 +15,20 @@ const ClockingsList = ({ loadClockings, clockings, history }) => {
     history.push('/clockings/new');
   };
 
+  console.log(clockings);
+
   return (
     <div>
       {/* <h1>Clockings</h1> */}
-      {clockings ? (
-        clockings.map((clocking) => (
-          <>
-            <ClockingListItem clocking={clocking} />
-          </>
-        ))
-      ) : (
-        <div>Still Loading</div>
-      )}
+      {/* {clockings ? ( */}
+      {/* clockings.map((clocking) => ( */}
+      {/* <> */}
+      {/* <ClockingListItem clocking={clocking} /> */}
+      {/* </> */}
+      {/* )) */}
+      {/* ) : ( */}
+      {/* <div>Still Loading</div> */}
+      {/* )} */}
       <button class="btn btn-primary" type="button" onClick={addClocking}>
         Add Clocking
       </button>
@@ -35,7 +37,7 @@ const ClockingsList = ({ loadClockings, clockings, history }) => {
 };
 
 const mapStateToProps = (state) => ({
-  clockings: state.clockings.clockings,
+  clockings: state.clockings.clocking,
   student: state.student.student,
 });
 
