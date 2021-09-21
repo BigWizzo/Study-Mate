de7aff495bd51872a8711b406b5b8095acd5756d
Will Nyamunokora <wnnwillard@gmail.com>
2021-09-21 04:43:14 +0200

    Test App.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from '../redux/store';
+import App from '../App';
+
+test('should render the Covid19-Catalogue-Board Application', () => {
+  const component = renderer.create(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
